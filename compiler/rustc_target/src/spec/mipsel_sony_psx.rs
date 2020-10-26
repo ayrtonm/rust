71a8539d85572a7ed90743520133c90f84add4e8
Ayrton <a.munoz3327@gmail.com>
2020-10-26 11:07:47 -0400

    Cleaned up target config file for psx and removed unnecessary changes to libs

--- compiler/rustc_target/src/spec/mipsel_sony_psx.rs
+++ compiler/rustc_target/src/spec/mipsel_sony_psx.rs
@@ -1,9 +1,12 @@
-use crate::spec::{LinkArgs, LinkerFlavor, LldFlavor, RelocModel};
-use crate::spec::{Target, TargetOptions};
+use crate::spec::{
+    LinkArgs, LinkerFlavor, LldFlavor, PanicStrategy, RelocModel, Target, TargetOptions,
+};
 
 // The PSX has custom linker requirements.
 const LINKER_SCRIPT: &str = include_str!("./mipsel_sony_psx_linker_script.ld");
 
+// These options are mostly taken from the mipsel-sony-psp and msp430-none-elf
+// configurations with some changes specific to MIPS I.
 pub fn target() -> Target {
     let mut pre_link_args = LinkArgs::new();
     pre_link_args.insert(LinkerFlavor::Lld(LldFlavor::Ld), vec!["--emit-relocs".to_string()]);
@@ -13,17 +16,17 @@ pub fn target() -> Target {
         target_endian: "little".to_string(),
         pointer_width: 32,
         target_c_int_width: "32".to_string(),
-        data_layout: "e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v64:64:64-v128:64:128-a:0:64-n32".to_string(),
+        data_layout: "e-m:m-p:32:32-i8:8:32-i16:16:32-i64:64-n32-S64".to_string(),
         arch: "mips".to_string(),
-        target_os: "psx".to_string(),
-        target_env: "".to_string(),
+        target_os: "none".to_string(),
+        target_env: "psx".to_string(),
         target_vendor: "sony".to_string(),
-        linker_flavor: LinkerFlavor::Lld(LldFlavor::Ld),
+        linker_flavor: LinkerFlavor::Ld,
 
         options: TargetOptions {
             cpu: "mips1".to_string(),
             executables: true,
-            linker: Some("rust-lld".to_owned()),
+            linker: Some("ld".to_owned()),
             linker_is_gnu: true,
             relocation_model: RelocModel::Static,
             eliminate_frame_pointer: false,
@@ -33,13 +36,20 @@ pub fn target() -> Target {
             // PSX doesn't natively support floats.
             features: "+soft-float".to_string(),
 
-            // PSP does not support trap-on-condition instructions.
-            //llvm_args: vec!["-mno-check-zero-division".to_string()],
+            // The MIPS I in the PSX doesn't have a SYNC instruction so we have
+            // to disable the Atomic* API.
+            // See https://github.com/rust-lang/rust/issues/54511 for more info.
+            max_atomic_width: Some(0),
+
+            // Taken from msp430-none-elf target configuration.
+            panic_strategy: PanicStrategy::Abort,
+            trap_unreachable: false,
+
+            // PSX does not support trap-on-condition instructions.
+            llvm_args: vec!["-mno-check-zero-division".to_string()],
             pre_link_args,
             link_script: Some(LINKER_SCRIPT.to_string()),
             ..Default::default()
         },
     }
 }
-    //"no-compiler-rt": true,
-    //"morestack": false,
